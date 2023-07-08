3ae89eae5db56ad8d8039333f75f67c1f8b67e01
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-09 00:26:54 +0530

    Update reviews.js

--- models/reviews.js
+++ models/reviews.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema({
   body: String,
   rating: { type: Number, min: 1, max: 5, default: 5 },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
