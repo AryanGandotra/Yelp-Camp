c690ea71821991e2d7f0e8a53e16b927ce65840a
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-09 00:26:57 +0530

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,27 +2,17 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const Campground = require("../models/campGround");
 const Review = require("../models/reviews");
-const { reviewSchema } = require("../schemas.js");
-const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
-
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+const { validateReview, isLogged, isReviewAuthor } = require("../middleware");
 
 router.post(
   "/",
   validateReview,
+  isLogged,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -32,7 +22,9 @@ router.post(
 );
 
 router.delete(
-  "/:reviewId",
+  "/:reviewId", 
+  isLogged,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
